15588bf26414ff433b703f81b706bd9e443b4bcd
Nishil1 <bharadwajn1136@masseyhigh.school.nz>
2022-03-04 13:01:43 +1300

    Create 02_input_checker.py

---
+++ 02_input_checker.py
@@ -0,0 +1,18 @@
+# checks users choice is integer, text or image
+def user_choice():
+
+	valid = False 
+	while not valid
+ 
+	response ="File type (integer / text/ image): ".lower()              
+
+	if response == "text" or == "t":
+		return response 
+
+	else:
+		print("Please choose a valid file type!")
+		print()
+
+
+		# Manin routine goes here
+		data_type = user_choice()
