d45a4ee834cd133490d209d45f4480dc7ab619d8
Nishil1 <bharadwajn1136@masseyhigh.school.nz>
2022-03-05 19:46:20 +1300

    Update 02_input_checker.py

--- 02_input_checker.py
+++ 02_input_checker.py
@@ -1,18 +1,37 @@
 # checks users choice is integer, text or image
 def user_choice():
 
-	valid = False 
-	while not valid
- 
-	response ="File type (integer / text/ image): ".lower()              
+	valid = True 
+	while valid:
+		response = input("File type (integer / text/ image): ").lower()  
+		
+		text_ok = ["text", "t", "txt"]
+		integer_ok = ["integer", "int", "#", "number"]
+		image_ok = ["image", "img", "pix", "picture", "pic"]
 
-	if response == "text" or == "t":
-		return response 
+		if response in text_ok:
+			return "text"
+		elif response in integer_ok:
+			return "integer"
+		elif response in image_ok:
+			return "image"
+		elif response == "i":
+			want_integer = input("Press <enter> for an integer or any key for image")
+			if want_integer == "":
+				return "integer"
+			else:
+				return "image"
 
-	else:
-		print("Please choose a valid file type!")
-		print()
+
+		else:
+			print("Please choose a valid file type!")
+			print()
 
 
-		# Manin routine goes here
-		data_type = user_choice()
+
+# Manin routine goes here
+keep_going = ""
+while keep_going == "":
+	
+	print("You chose file type: ", user_choice())  
+	print()																						
