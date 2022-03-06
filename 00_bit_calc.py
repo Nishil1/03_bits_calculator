c744da0a1ed60319d8af3daf3594a7276b503d09
Nishil1 <bharadwajn1136@masseyhigh.school.nz>
2022-03-07 12:43:26 +1300

    Update 00_bit_calc.py

--- 00_bit_calc.py
+++ 00_bit_calc.py
@@ -43,6 +43,86 @@ def user_choice():
 			print()
 
 
+
+def num_check(question, low):
+	valid = False
+	while not valid:
+		
+		error=" please enter an integer that is more than or equal to {}".format(low)
+		
+		try: 
+			# ask user to enter a number
+
+			response = int(input(question))
+			
+			# check if the number is more than 0
+			
+			if response >= low:
+				return response  
+
+			#outputs error if the input is invalid
+
+			else:
+				print(error)
+				print()
+		except ValueError: 
+			print(error)
+
+
+def text_bits():
+
+
+    print()
+
+    var_text = input("Enter some text: ")
+    var_lenght = len(var_text)
+    num_bits = 8 * var_lenght 
+
+    print()
+    print("\'{}\' has {} chracters...".format(var_text, var_lenght))
+    print("# of bits is {} * 8...".format(var_lenght))
+    print("We need {} of bits to represent {}".format(num_bits, var_text))
+    print()
+
+    return ""
+
+
+def image_bits():
+
+	image_width = num_check("Image Width? ", 1)
+	image_height = num_check("Image Height? ", 1)
+
+	num_pixels = image_width * image_height
+	num_bits = num_pixels * 24
+
+	print()
+	print("# of pixels = {} * {} = {}". format(image_height, image_width, num_pixels))
+	print("# of bits = {} * 24 = {}".format (num_pixels, num_bits))
+
+	image_bits()
+
+
+
+
+def int_bits():
+
+	var_integer = num_check("Please enter an integer: ", 0)
+
+	var_binary = "{0:b}".format (var_integer)
+
+	num_bits = len(var_binary)
+
+	print()
+	print("{} in binary is {}".format(var_integer, var_binary))
+	print("# of bits is {}".format(num_bits))
+	print()
+
+	return ""
+
+
+
+
+
 # Main routine goes here
 
 statement_generator("Bit calculator for integers, text & images", "-")
@@ -51,6 +131,21 @@ keep_going = ""
 while keep_going == "":
 	data_type = user_choice()
 	print("You chose", data_type)
+
+	if data_type == "integer":
+		int_bits()
+	elif data_type == "image":
+		image_bits()
+	else:
+		text_bits()
+
+		print()
+		keep_going = input("Press <enter> to continue or any key to quit")
+		print()
+
+
+
+
 
 
 
